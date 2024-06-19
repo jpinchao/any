c7b4ea8aa1916de189d65bb04fbd52db687e940d
José Raúl Pinchao Cuatín <jpinchao@unal.edu.co>
2024-06-19 10:40:42 -0500

    Add files via upload

---
+++ anydesk_raspi_bullseye.sh
@@ -0,0 +1,3 @@
+apt install libgles-dev libegl-dev
+sudo ln -s /usr/lib/arm-linux-gnueabihf/libGLESv2.so /usr/lib/libbrcmGLESv2.so
+sudo ln -s /usr/lib/arm-linux-gnueabihf/libEGL.so /usr/lib/libbrcmEGL.so
